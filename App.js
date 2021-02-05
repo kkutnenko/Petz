477cbc91af2d93afa2574f3275f1df970927e80c
Kirill Kutnenko <kkutnenko@my.bcit.ca>
2021-02-05 00:22:30 -0800

    completed app

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
+import Pet from './src/components/PetDetail';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+    <ScrollView >
+      <Pet name="Muffin" imageSrc={require('./assets/cat1.jpg')}/>
+      <Pet name="Sir Charles Flooferson III" imageSrc={require('./assets/cat2.jpg')}/>
+      <Pet name="Cookie" imageSrc={require('./assets/cat3.jpg')}/>
+    </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: Constants.statusBarHeight,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
